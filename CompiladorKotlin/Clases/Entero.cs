140f43d762a9152398df6e00f5fc51c658b5638b
david-o-rosario1201 <finnnn023@gmail.com>
2024-06-22 13:46:13 -0400

    Add project files.

---
+++ CompiladorKotlin/Clases/Entero.cs
@@ -0,0 +1,18 @@
+﻿namespace CompiladorKotlin.Clases;
+
+public class Entero
+{
+	public string nombre { get; set; }
+	public int valor { get; set; }
+
+	public Entero(string nombre, int valor)
+	{
+		this.nombre = nombre;
+		this.valor = valor;
+	}
+
+	public Entero(string nombre)
+	{
+		this.nombre = nombre;
+	}
+}
---
+++ CompiladorKotlin/Clases/Short.cs
@@ -0,0 +1,18 @@
+﻿namespace CompiladorKotlin.Clases;
+
+public class Short
+{
+	public string nombre { get; set; }
+	public short valor { get; set; }
+
+	public Short(string nombre, short valor)
+	{
+		this.nombre = nombre;
+		this.valor = valor;
+	}
+
+	public Short(string nombre)
+	{
+		this.nombre = nombre;
+	}
+}
